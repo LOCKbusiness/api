6fb7d0660c5b0d780157b62df6820fd10d98c13e
Krysh <krysh.developer@gmail.com>
2022-09-16 09:26:33 +0200

    [DEV-721] reverted hello world message to fix tests

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello in the World of LOCK.space';
+    return 'Hello World!';
   }
 }
