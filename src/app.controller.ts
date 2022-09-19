3d79595fb9f20a62c81543e5c073005377e5c750
Krysh <krysh.developer@gmail.com>
2022-09-19 09:42:24 +0200

    [DEV-790] added swagger redirect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller('app')
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Redirect('swagger')
+  @ApiExcludeEndpoint()
+  async home(): Promise<any> {
+    // nothing to do (redirect to Swagger UI)
+  }
+
+  @Get('app')
   getHello(): string {
     return this.appService.getHello();
   }
