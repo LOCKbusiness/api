65879587bcdc57f401c79c275bfddb7d8a9c92d8
Anton Semikin <daotreeproject@gmail.com>
2022-09-19 18:05:22 +0200

    [DEV-757] added deposit and withdrawal basic statuses

--- src/subdomains/staking/domain/entities/deposit.entity.ts
+++ src/subdomains/staking/domain/entities/deposit.entity.ts
@@ -2,13 +2,16 @@ import { Asset } from 'src/shared/entities/asset.entity';
 import { IEntity } from 'src/shared/entities/entity';
 import { Column, Entity, ManyToOne } from 'typeorm';
 import { BlockchainAddress } from '../../../../shared/entities/blockchain-address.entity';
-import { StakingAddressPurposes } from '../enums';
+import { DepositStatus, StakingAddressPurposes } from '../enums';
 import { Staking } from './staking.entity';
 
 @Entity()
 export class Deposit extends IEntity {
   @ManyToOne(() => Staking, (staking) => staking.deposits, { eager: true, nullable: true })
   staking: Staking;
+
+  @Column({ length: 256, nullable: false })
+  status: DepositStatus;
 
   @ManyToOne(() => Asset, { eager: true, nullable: false })
   asset: Asset;
