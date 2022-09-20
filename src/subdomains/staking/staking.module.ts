de94320e0400f34276651deea90c3026a59ffd1b
Kolibri <unknown>
2022-09-20 03:58:35 +0200

    [DEV-756] Add staking module imports/providers

--- src/subdomains/staking/staking.module.ts
+++ src/subdomains/staking/staking.module.ts
@@ -2,16 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AinModule } from 'src/blockchain/ain/ain.module';
 import { SharedModule } from 'src/shared/shared.module';
+import { UserModule } from '../user/user.module';
 import { DepositController } from './api/controllers/deposit.controller';
 import { StakingController } from './api/controllers/staking.controller';
+import { StakingFactory } from './application/factories/staking.factory';
 import { StakingRepository } from './application/repositories/staking.repository';
 import { StakingDepositService } from './application/services/staking-deposit.service';
 import { StakingService } from './application/services/staking.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StakingRepository]), AinModule, SharedModule],
+  imports: [TypeOrmModule.forFeature([StakingRepository]), AinModule, SharedModule, UserModule],
   controllers: [StakingController, DepositController],
-  providers: [StakingService, StakingDepositService],
+  providers: [StakingService, StakingDepositService, StakingFactory],
   exports: [],
 })
 export class StakingModule {}
