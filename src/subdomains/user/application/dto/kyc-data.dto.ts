706c17ea0df1c807ecd3c1cc1f739fd2457bfcc5
Kolibri <>
2022-09-21 16:23:05 +0200

    [DEV-756] Refactoring webhook route

--- src/subdomains/user/application/dto/kyc-data.dto.ts
+++ src/subdomains/user/application/dto/kyc-data.dto.ts
@@ -1,77 +1,61 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { KycResult, KycStatus } from '../../domain/enums';
 
 export class KycWebhookDto {
-  @ApiProperty()
+  @IsNotEmpty()
   @IsEnum(KycResult)
   result: KycResult;
 
-  @ApiProperty()
+  @IsOptional()
   data: KycDataDto;
 
-  @ApiProperty()
+  @IsOptional()
   @IsString()
   reason: string;
 }
 
 export class KycDataDto {
-  @ApiProperty()
   @IsOptional()
   @IsString()
   kycId: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   mail: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   firstName: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   lastName: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   street: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   houseNumber: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   city: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   zip: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsString()
   phone: string;
 
-  @ApiProperty()
   @IsOptional()
   @IsEnum(KycStatus)
   kycStatus: KycStatus;
 
-  @ApiProperty()
+  @IsNotEmpty()
   @IsString()
   kycHash: string;
-
-  @ApiProperty()
-  @IsOptional()
-  @IsString()
-  message: string;
 }
