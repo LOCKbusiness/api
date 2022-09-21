afd6a97f597c67b9dad530ae70e96fb0c4dbe18b
Kolibri <>
2022-09-21 17:37:58 +0200

    [FIX] Add default spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AinModule } from './blockchain/ain/ain.module';
 import { GetConfig } from './config/config';
 import { SharedModule } from './shared/shared.module';
-import { StakingModule } from './subdomains/staking/staking.module';
-import { UserModule } from './subdomains/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(GetConfig().database), SharedModule, AinModule, UserModule, StakingModule],
+  imports: [TypeOrmModule.forRoot(GetConfig().database), SharedModule, AinModule],
   controllers: [AppController],
-  providers: [],
+  providers: [AppService],
 })
 export class AppModule {}
