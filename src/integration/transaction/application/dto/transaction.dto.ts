8c7f536aa1a13b25659d00a3dcdd5c30bf0fd2a8
Krysh <krysh.developer@gmail.com>
2022-10-08 16:18:00 +0200

    [DEV-841] CRC

--- src/integration/transaction/application/dto/transaction.dto.ts
+++ src/integration/transaction/application/dto/transaction.dto.ts
@@ -1,23 +1,9 @@
-import { IsNotEmpty, IsObject, IsOptional, IsString, IsUUID } from 'class-validator';
 import { RawTxDto } from 'src/blockchain/ain/jellyfish/dto/raw-tx.dto';
 
 export class TransactionDto {
-  @IsNotEmpty()
-  @IsUUID()
   id: string;
-
-  @IsNotEmpty()
-  @IsString()
   issuerSignature: string;
-
-  @IsOptional()
-  @IsString()
   verifierSignature?: string;
-
-  @IsNotEmpty()
-  @IsObject()
   rawTx: RawTxDto;
-
-  @IsOptional()
   payload?: any;
 }
