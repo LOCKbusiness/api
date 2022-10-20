1366cc3d0b08fe1c1fd176f8c51184aa092cc43c
Krysh <krysh.developer@gmail.com>
2022-10-20 14:21:11 +0200

    [DEV-882] CRC

--- migration/1666189378543-transaction-in-blockchain.js
+++ migration/1666189378543-transaction-in-blockchain.js
@@ -4,7 +4,7 @@ module.exports = class transactionInBlockchain1666189378543 {
     name = 'transactionInBlockchain1666189378543'
 
     async up(queryRunner) {
-        await queryRunner.query(`EXEC sp_rename "sqldb-lock-api-dev.dbo.transaction.active", "inBlockchain"`);
+        await queryRunner.query(`EXEC sp_rename "transaction.active", "inBlockchain"`);
         await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "DF_30986c4e2867379118019886d2c"`);
         await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "DF_440e43573633842aab528d19c49" DEFAULT 0 FOR "inBlockchain"`);
     }
@@ -12,6 +12,6 @@ module.exports = class transactionInBlockchain1666189378543 {
     async down(queryRunner) {
         await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "DF_440e43573633842aab528d19c49"`);
         await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "DF_30986c4e2867379118019886d2c" DEFAULT 1 FOR "inBlockchain"`);
-        await queryRunner.query(`EXEC sp_rename "sqldb-lock-api-dev.dbo.transaction.inBlockchain", "active"`);
+        await queryRunner.query(`EXEC sp_rename "transaction.inBlockchain", "active"`);
     }
 }
