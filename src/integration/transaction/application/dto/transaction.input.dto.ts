e2bbf18e7a62bb134c6825235e9cc1ffb0e7de9e
Krysh <krysh.developer@gmail.com>
2022-11-04 09:11:12 +0100

    [DEV-975] added all definitions for input parameters

--- src/integration/transaction/application/dto/transaction.input.dto.ts
+++ src/integration/transaction/application/dto/transaction.input.dto.ts
@@ -12,8 +12,60 @@ export class TransactionInputDto {
   parameters: any;
 }
 
-export interface TakeLoanInputDto {
+export interface SendTokenParameters {
+  from: string;
+  to: string;
+  token: string;
+  amount: number;
+}
+
+export interface CreateVaultParameters {
+  address: string;
+}
+
+export interface VaultParameters {
   address: string;
   vault: string;
-  amount: BigNumber;
+}
+
+export interface DepositToVaultParameters extends VaultParameters {
+  token: string;
+  amount: number;
+}
+
+export interface WithdrawFromVaultParameters extends VaultParameters {
+  token: string;
+  amount: number;
+}
+
+export interface TakeLoanParameters extends VaultParameters {
+  amount: number;
+}
+
+export interface PaybackLoanParameters extends VaultParameters {
+  amount: number;
+}
+
+export interface TokenBalanceParameters {
+  token: string;
+  amount: number;
+}
+
+export interface AddPoolLiquidityParameters {
+  address: string;
+  partA: TokenBalanceParameters;
+  partB: TokenBalanceParameters;
+}
+
+export interface RemovePoolLiquidityParameters {
+  address: string;
+  poolPair: string;
+  amount: number;
+}
+
+export interface CompositeSwapParameters {
+  address: string;
+  fromToken: string;
+  amount: number;
+  toToken: string;
 }
