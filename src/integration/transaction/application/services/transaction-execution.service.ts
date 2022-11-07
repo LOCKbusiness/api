a466a45975c872cb7f083480e38a9d65aa7c100e
Krysh <krysh.developer@gmail.com>
2022-11-07 11:38:55 +0100

    [DEV-975] added tx type to payload

--- src/integration/transaction/application/services/transaction-execution.service.ts
+++ src/integration/transaction/application/services/transaction-execution.service.ts
@@ -29,6 +29,7 @@ import {
 import { TransactionService } from './transaction.service';
 import { WIF } from '@defichain/jellyfish-crypto';
 import { CryptoService } from 'src/blockchain/shared/services/crypto.service';
+import { TransactionType } from '../../domain/enums';
 
 @Injectable()
 export class TransactionExecutionService {
@@ -51,43 +52,43 @@ export class TransactionExecutionService {
   async createMasternode(data: CreateMasternodeData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForCreate(data.masternode);
     console.info(`Create masternode tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.CREATE_MASTERNODE));
   }
 
   async resignMasternode(data: ResignMasternodeData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForResign(data.masternode);
     console.info(`Resign masternode tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.RESIGN_MASTERNODE));
   }
 
   async sendFromLiq(data: SendFromLiqData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForSendFromLiq(data.to, data.amount, data.sizePriority);
     console.info(`Send from liq tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.SEND_FROM_LIQ));
   }
 
   async sendFromLiqToCustomer(data: SendFromLiqToCustomerData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForSendFromLiq(data.to, data.amount, UtxoSizePriority.FITTING);
     console.info(`Send from liq to customer tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, { id: data.withdrawalId });
+    return this.signAndBroadcast(rawTx, { id: data.withdrawalId, type: TransactionType.WITHDRAWAL });
   }
 
   async sendToLiq(data: SendToLiqData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForSendToLiq(data.from, data.amount);
     console.info(`Send to liq tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.SEND_TO_LIQ));
   }
 
   async splitBiggestUtxo(data: SplitData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForSplitUtxo(data.address, data.split);
     console.info(`Split tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx);
+    return this.signAndBroadcast(rawTx, { type: TransactionType.UTXO_SPLIT });
   }
 
   async mergeSmallestUtxos(data: MergeData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForMergeUtxos(data.address, data.merge);
     console.info(`Merge tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx);
+    return this.signAndBroadcast(rawTx, { type: TransactionType.UTXO_MERGE });
   }
 
   async sendToken(data: SendTokenData): Promise<string> {
@@ -98,37 +99,37 @@ export class TransactionExecutionService {
       data.balance.amount,
     );
     console.info(`Send account tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx);
+    return this.signAndBroadcast(rawTx, { type: TransactionType.ACCOUNT_TO_ACCOUNT });
   }
 
   async createVault(data: CreateVaultData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForCreateVault(data.owner);
     console.info(`Create vault tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.CREATE_VAULT));
   }
 
   async depositToVault(data: DepositToVaultData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForDepositToVault(data.from, data.vault, data.token, data.amount);
     console.info(`Deposit to vault tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.DEPOSIT_TO_VAULT));
   }
 
   async withdrawFromVault(data: WithdrawFromVaultData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForWithdrawFromVault(data.to, data.vault, data.token, data.amount);
     console.info(`Withdraw from vault tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.WITHDRAW_FROM_VAULT));
   }
 
   async takeLoan(data: TakeLoanData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForTakeLoan(data.to, data.vault, data.token, data.amount);
     console.info(`Take loan tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.TAKE_LOAN));
   }
 
   async paybackLoan(data: PaybackLoanData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForPaybackLoan(data.from, data.vault, data.token, data.amount);
     console.info(`Payback loan tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.PAYBACK_LOAN));
   }
 
   async addPoolLiquidity(data: AddPoolLiquidityData): Promise<string> {
@@ -140,13 +141,13 @@ export class TransactionExecutionService {
       data.partB.amount,
     );
     console.info(`Add pool liquidity tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.POOL_ADD_LIQUIDITY));
   }
 
   async removePoolLiquidity(data: RemovePoolLiquidityData): Promise<string> {
     const rawTx = await this.jellyfishService.rawTxForRemovePoolLiquidity(data.from, data.token, data.amount);
     console.info(`Remove pool liquidity tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.POOL_REMOVE_LIQUIDITY));
   }
 
   async compositeSwap(data: CompositeSwapData): Promise<string> {
@@ -157,11 +158,12 @@ export class TransactionExecutionService {
       data.destination.token,
     );
     console.info(`Composite swap tx ${rawTx.id}`);
-    return this.signAndBroadcast(rawTx, this.createPayloadFor(data));
+    return this.signAndBroadcast(rawTx, this.createPayloadFor(data, TransactionType.COMPOSITE_SWAP));
   }
 
-  private createPayloadFor(data: WalletBaseData): any {
+  private createPayloadFor(data: WalletBaseData, type: TransactionType): any {
     return {
+      type,
       ownerWallet: data.ownerWallet,
       accountIndex: data.accountIndex,
     };
