75a318f63368039f44755c19954a0aaf7024464c
Yannick1712 <nickbusch1998@gmail.com>
2022-11-07 19:28:10 +0100

    [DEV-998] Update Mail system

--- src/integration/notification/entities/mail/user-mail.ts
+++ src/integration/notification/entities/mail/user-mail.ts
@@ -1,3 +1,4 @@
+import { Config } from 'src/config/config';
 import { User } from 'src/subdomains/user/domain/entities/user.entity';
 import { NotificationMetadata, NotificationOptions } from '../notification.entity';
 import { Mail } from './base/mail';
@@ -13,12 +14,19 @@ export interface UserMailParams {
   subject: string;
   salutation: string;
   body: string;
+  telegramUrl?: string;
+  twitterUrl?: string;
   metadata?: NotificationMetadata;
   options?: NotificationOptions;
 }
 
 export class UserMail extends Mail {
   constructor(params: UserMailParams) {
-    super({ ...params, template: 'default' });
+    const defaultParams: Partial<UserMailParams> = {
+      twitterUrl: Config.defaultTwitterUrl,
+      telegramUrl: Config.defaultTelegramUrl,
+    };
+
+    super({ ...params, template: 'default', templateParams: { ...defaultParams, ...params } });
   }
 }
