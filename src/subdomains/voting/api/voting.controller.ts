cf9ea2431161a0e2ad81d2848ae8915106308438
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2022-11-08 23:50:44 +0100

    [DEV-1006] Add cfp votes (#107)
    * [DEV-1006] Add firstBlockFound to masternode table, masternodeBlockCheck and migration
    
    * [DEV-1006] Add cfp voting
    
    * [DEV-1006] Change column length
    
    Co-authored-by: Kolibri <>

---
+++ src/subdomains/voting/api/voting.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { GetJwt } from 'src/shared/auth/get-jwt.decorator';
+import { JwtPayload } from 'src/shared/auth/jwt-payload.interface';
+import { RoleGuard } from 'src/shared/auth/role.guard';
+import { WalletRole } from 'src/shared/auth/wallet-role.enum';
+import { UserService } from 'src/subdomains/user/application/services/user.service';
+import { Votes } from '../application/dto/votes.dto';
+
+@ApiTags('Voting')
+@Controller('voting')
+export class VotingController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('votes')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), new RoleGuard(WalletRole.USER))
+  async getVotes(@GetJwt() jwt: JwtPayload): Promise<Votes> {
+    return this.userService.getVotes(jwt.userId);
+  }
+
+  @Put('votes')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard(), new RoleGuard(WalletRole.USER))
+  async updateVotes(@GetJwt() jwt: JwtPayload, @Body() votes: Votes): Promise<Votes> {
+    return this.userService.updateVotes(jwt.userId, votes);
+  }
+}
