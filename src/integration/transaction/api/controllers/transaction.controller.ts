f0276349d1c4a7ea0eb0383c4902f8b9d4b44f85
David May <david.leo.may@gmail.com>
2022-11-15 17:46:30 +0100

    [DEV-789] Added monitoring ping

--- src/integration/transaction/api/controllers/transaction.controller.ts
+++ src/integration/transaction/api/controllers/transaction.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Param, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiExcludeEndpoint, ApiTags } from '@nestjs/swagger';
 import { RoleGuard } from 'src/shared/auth/role.guard';
 import { WalletRole } from 'src/shared/auth/wallet-role.enum';
+import { MonitoringService } from 'src/subdomains/monitoring/application/services/monitoring.service';
 import { InvalidateDto } from '../../application/dto/invalidate.dto';
 import { SignatureDto } from '../../application/dto/signature.dto';
 import { SignedTransactionDto } from '../../application/dto/signed-transaction.dto';
@@ -12,7 +13,10 @@ import { TransactionService } from '../../application/services/transaction.servi
 @ApiTags('Transaction')
 @Controller('transaction')
 export class TransactionController {
-  constructor(private readonly transactionService: TransactionService) {}
+  constructor(
+    private readonly transactionService: TransactionService,
+    private readonly monitoring: MonitoringService,
+  ) {}
 
   @Get('open')
   @ApiBearerAuth()
@@ -34,23 +38,38 @@ export class TransactionController {
   @ApiBearerAuth()
   @ApiExcludeEndpoint()
   @UseGuards(AuthGuard(), new RoleGuard(WalletRole.TRANSACTION_CHECKER))
-  verifyTransaction(@Param('id') id: string, @Body() dto: SignatureDto) {
-    this.transactionService.verified(id, dto.signature);
+  async verifyTransaction(@Param('id') id: string, @Body() dto: SignatureDto, @Headers('Device-Id') deviceId: string) {
+    await this.transactionService.verified(id, dto.signature);
+    await this.onPing('tc-' + deviceId);
   }
 
   @Put(':id/invalidated')
   @ApiBearerAuth()
   @ApiExcludeEndpoint()
   @UseGuards(AuthGuard(), new RoleGuard(WalletRole.TRANSACTION_CHECKER))
-  invalidateTransaction(@Param('id') id: string, @Body() dto: InvalidateDto) {
-    this.transactionService.invalidated(id, dto.reason);
+  async invalidateTransaction(
+    @Param('id') id: string,
+    @Body() dto: InvalidateDto,
+    @Headers('Device-Id') deviceId: string,
+  ) {
+    await this.transactionService.invalidated(id, dto.reason);
+    await this.onPing('tc-' + deviceId);
   }
 
   @Put(':id/signed')
   @ApiBearerAuth()
   @ApiExcludeEndpoint()
   @UseGuards(AuthGuard(), new RoleGuard(WalletRole.TRANSACTION_SIGNER))
-  signTransaction(@Param('id') id: string, @Body() dto: SignedTransactionDto) {
-    this.transactionService.signed(id, dto.hex);
+  async signTransaction(
+    @Param('id') id: string,
+    @Body() dto: SignedTransactionDto,
+    @Headers('Device-Id') deviceId: string,
+  ) {
+    await this.transactionService.signed(id, dto.hex);
+    await this.onPing('cw-' + deviceId);
+  }
+
+  private onPing(device: string): Promise<void> {
+    return this.monitoring.onWebhook('staking', 'external', device);
   }
 }
