2d4eaf2352d7759ea46dcd96496343b2013c4faf
Krysh <krysh.developer@gmail.com>
2022-12-23 18:03:14 +0100

    [DEV-1166] Endpoint to retrieve all vaults (#184)
    * [DEV-1166] added endpoint to retrieve all vaults
    
    * [DEV-1166] Moved to vault domain
    
    Co-authored-by: David May <david.leo.may@gmail.com>

--- src/integration/vault/vault.module.ts
+++ src/integration/vault/vault.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SharedModule } from 'src/shared/shared.module';
+import { VaultController } from './api/vault.controller';
 import { VaultRepository } from './application/repositories/vault.repository';
 import { VaultService } from './application/services/vault.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([VaultRepository]), SharedModule],
-  controllers: [],
+  controllers: [VaultController],
   providers: [VaultService],
   exports: [VaultService],
 })
