e37b563800db371e4e6a406d5df2a626c206efbe
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2023-01-23 00:18:24 +0100

    [FEATURE] Add getMasternodes owner

--- src/subdomains/analytics/api/controllers/staking-analytics.controller.ts
+++ src/subdomains/analytics/api/controllers/staking-analytics.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { MasternodeService } from 'src/integration/masternode/application/services/masternode.service';
 import { StakingDepositService } from 'src/subdomains/staking/application/services/staking-deposit.service';
 import { StakingWithdrawalService } from 'src/subdomains/staking/application/services/staking-withdrawal.service';
 import { StakingAnalyticsQuery } from '../../application/dto/input/staking-analytics-query.dto';
 import { StakingAnalyticsOutputDto } from '../../application/dto/output/staking-analytics.output.dto';
+import { StakingAnalyticsMasternodesOutputDto } from '../../application/dto/output/staking-analytics.omasternodes.utput.dto';
 import { TimeSpanDto } from '../../application/dto/output/time-span.dto';
 import { StakingTransactionDto } from '../../application/dto/output/transactions.dto';
 import { StakingAnalyticsService } from '../../application/services/staking-analytics.service';
@@ -15,6 +17,7 @@ export class StakingAnalyticsController {
     private readonly stakingAnalyticsService: StakingAnalyticsService,
     private readonly stakingDepositService: StakingDepositService,
     private readonly stakingWithdrawalService: StakingWithdrawalService,
+    private readonly masternodeService: MasternodeService,
   ) {}
 
   @Get()
@@ -30,5 +33,11 @@ export class StakingAnalyticsController {
       deposits: await this.stakingDepositService.getDeposits(dateFrom, dateTo),
       withdrawals: await this.stakingWithdrawalService.getWithdrawals(dateFrom, dateTo),
     };
+  }
+
+  @Get('masternodes')
+  async getMasternodes(): Promise<StakingAnalyticsMasternodesOutputDto[]> {
+    const masternodes = await this.masternodeService.getActive();
+    return Promise.all(masternodes.map((m) => ({ owner: m.owner })));
   }
 }
