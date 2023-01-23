e0e62d289736c9b53ce99233e19a1c293aff008c
Kolibri1990 <66674482+Kolibri1990@users.noreply.github.com>
2023-01-23 12:00:18 +0100

    [FEATURE] Refactoring

--- src/integration/masternode/application/services/masternode.service.ts
+++ src/integration/masternode/application/services/masternode.service.ts
@@ -186,6 +186,10 @@ export class MasternodeService {
     });
   }
 
+  async getAllOwner(): Promise<string[]> {
+    return await this.getActive().then((masternodes) => masternodes.map((v) => v.owner));
+  }
+
   async filterByBlockchainState(masternodes: Masternode[], state: BlockchainMasternodeState): Promise<Masternode[]> {
     const infos = await Promise.all(masternodes.map((mn) => this.getMasternodeStates(mn.creationHash)));
     return masternodes.filter((mn) => state === infos.find((info) => info.hash === mn.creationHash).state);
--- src/subdomains/analytics/application/dto/output/staking-analytics.omasternodes.utput.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class StakingAnalyticsMasternodesOutputDto {
-  @ApiProperty()
-  owner: string;
-}
