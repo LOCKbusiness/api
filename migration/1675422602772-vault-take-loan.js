32a33846119ead2f067b4e230f7a8de7a0e483ab
Kolibri <>
2023-02-03 12:12:36 +0100

    [DEV-1311] fix migration

--- migration/1675422602772-vault-take-loan.js
+++ migration/1675422602772-vault-take-loan.js
@@ -4,10 +4,10 @@ module.exports = class vaultTakeLoan1675422602772 {
     name = 'vaultTakeLoan1675422602772'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "sqldb-lock-api-dev"."dbo"."vault" ADD "takeLoanAddress" nvarchar(255)`);
+        await queryRunner.query(`ALTER TABLE "dbo"."vault" ADD "takeLoanAddress" nvarchar(255)`);
     }
 
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "sqldb-lock-api-dev"."dbo"."vault" DROP COLUMN "takeLoanAddress"`);
+        await queryRunner.query(`ALTER TABLE "dbo"."vault" DROP COLUMN "takeLoanAddress"`);
     }
 }
